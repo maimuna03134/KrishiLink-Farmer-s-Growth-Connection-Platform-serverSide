c5e2c3e82f8159cc1923c2518039a0e1d53e8c8e
maimuna03134 <mj9644212@gmail.com>
2025-11-11 19:20:39 +0600

    update some data of single data method

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
     app.get("/crops/:id", async (req, res) => {
       const { id } = req.params;
       console.log(id);
-      const query = { _id: new ObjectId(id) };
+      const query = { _id: id };
       const result = await cropsCollection.findOne(query);
       res.send({
         success: true,
