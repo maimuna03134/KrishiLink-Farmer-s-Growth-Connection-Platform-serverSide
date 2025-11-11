cdf43c8277de64898f678005d8ec72ceec2b7291
maimuna03134 <mj9644212@gmail.com>
2025-11-11 14:41:18 +0600

    added get method

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+// mongodb
+const uri =
+  "mongodb+srv://crops-db:zu5mEPXxBTYpD8tZ@cluster0.imnpg23.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("crops-db");
+    const cropsCollection = db.collection("crops");
+
+    app.get("/crops", async (req, res) => {
+      const result = await cropsCollection.find().toArray();
+      res.send(result);
+    });
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
