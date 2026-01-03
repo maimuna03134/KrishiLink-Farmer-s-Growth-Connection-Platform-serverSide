42287352e1f9ef34243f28602554f6cf3ac9e7b5
maimuna03134 <mj9644212@gmail.com>
2026-01-03 15:57:10 +0600

    Fix vercel express server

--- index.js
+++ index.js
@@ -516,6 +516,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+module.exports = app;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
